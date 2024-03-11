3237eb62f68033c37565150669da9c0fa7dadccf
Muhammet Kus <muhametkus@gmail.com>
2024-03-11 13:15:00 +0300

    updatee

--- components/Projelerim.tsx
+++ components/Projelerim.tsx
@@ -24,7 +24,7 @@ const Projelerim = () => {
     <div className='w-full flex flex-col gap-20 2xl:gap-32 md:gap-14  xl:grid xl:grid-cols-2 2xl:grid 2xl:grid-cols-2 pb-20'>
 
 {projeler.map((proje)=>(
-    <div className="card w-[98%] sm:w-[85%] sm:mx-auto glass border-0">
+    <div className="card w-[98%] sm:w-[95%] sm:mx-auto glass border-0">
     <figure  className={`h-[200px] px-10 ${proje.renkkodu}`}><img src={proje.logo} alt="car!"/></figure>
     <div className="card-body bg-[#B9B4C7] rounded-b-2xl">
       <a href={`https://${proje.link}`} className="text-center text-2xl text-[#352F44]" target='_blank'>{proje.link}</a>
